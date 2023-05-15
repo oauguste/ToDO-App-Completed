83cdd306fb937ce9a7a487cddb0c966ecf1c777b
oauguste <augusteosnac@gmail.com>
2023-05-15 14:06:21 -0400

    init setup

---
+++ ToDo-Application/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  mode: "jit",
+  purge: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
