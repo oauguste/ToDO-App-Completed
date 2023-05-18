2ed9e22bd612701690e71468b30c45961ea98ba5
oauguste <augusteosnac@gmail.com>
2023-05-18 10:37:29 -0400

    partially completed

--- ToDo-Application/main.js
+++ ToDo-Application/main.js
@@ -1,4 +1,4 @@
-// import "./style.css";
+import "./style.css";
 
 class Note {
   constructor(content) {
@@ -22,6 +22,7 @@ class Folder {
   getFolderName() {
     return this.folderName;
   }
+  //Notes Array
   getNotes() {
     return this.notes;
   }
@@ -47,6 +48,7 @@ class FolderSystem {
   constructor(folders) {
     this.folders = folders;
   }
+  //folder array
   getFolders() {
     return this.folders;
   }
@@ -63,5 +65,105 @@ class FolderSystem {
     return this.folders;
   }
 }
+// module.exports = { Note, Folder, FolderSystem };
+const folderForm = document.querySelector("#folderForm");
+const folderInput = document.querySelector("#folderInput");
 
-module.exports = { Note, Folder, FolderSystem };
+const folderBtn = document.querySelector(
+  "#createFolderBtn"
+);
+const noteForm = document.querySelector("#notesForm");
+const noteInput = document.querySelector("#notesInput");
+
+const noteButton = document.querySelector("#noteButton");
+const noteContainer = document.querySelector(
+  "#noteContainer"
+);
+
+function renderNotes(array, parent) {
+  parent.innerHTML = "";
+  array.forEach((note) => {
+    const noteEl = document.createElement("p");
+    const doneBtn = document.createElement("button");
+    const editBtn = document.createElement("button");
+    const deleteBtn = document.createElement("button");
+
+    doneBtn.classList.add(
+      "p-2",
+      "bg-orange-200",
+      "rounded"
+    );
+    editBtn.classList.add("p-2", "bg-blue-200", "rounded");
+    deleteBtn.classList.add("p-2", "bg-red-200", "rounded");
+    noteEl.classList.add(
+      "p-2",
+      "text-white",
+      "bg-black",
+      "rounded"
+    );
+    noteEl.textContent = note.getNoteContent();
+
+    parent.appendChild(noteEl);
+    parent.appendChild(doneBtn).textContent = "done";
+    parent.appendChild(editBtn).textContent = "edit";
+    parent.appendChild(deleteBtn).textContent = "delete";
+  });
+}
+
+function createBtn(name, parent, folder) {
+  const button = document.createElement("button");
+  button.classList.add(
+    "p-2",
+    "mt-2",
+    "ml-auto",
+    "mr-auto",
+    "text-sm",
+    "text-black",
+    "bg-blue-200",
+    "rounded"
+  );
+  button.textContent = name;
+  parent.appendChild(button);
+
+  //render notes
+  button.addEventListener("click", () => {
+    console.log(
+      "Button clicked. Folder name:",
+      folder.notes
+    );
+    renderNotes(folder.getNotes(), noteContainer);
+  });
+}
+
+function createFolder() {
+  const inputValue = folderInput.value.trim();
+  if (inputValue !== "") {
+    const folder = new Folder(inputValue, []);
+    console.log("New folder created:", folder);
+    folderInput.value = "";
+    createBtn(folder.getFolderName(), folderForm, folder);
+  }
+}
+
+folderForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  createFolder();
+});
+
+{
+  /* <div id="note" class="flex flex-wrap gap-2 p-1 m-2">
+  <p class="p-2 text-white bg-black rounded">
+    Hello Im a note Lorem, ipsum. Lorem ipsum dolor sit.
+    Lorem ipsum dolor sit amet
+  </p>
+  <button id="done" class="p-2 bg-orange-200 rounded">
+    done
+  </button>
+  <button id="edit" class="p-2 bg-blue-200 rounded">
+    edit
+  </button>
+  <button id="delete" class="p-2 bg-red-200 rounded">
+    delete
+  </button>
+</div>; */
+}
